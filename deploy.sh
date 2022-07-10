ca24bd99adb7be460f7cf5381dc2066dfb970e44
zhoubichuan <zhoubichuan@icloud.com>
2022-07-10 12:16:51 +0800

    master

--- deploy.sh
+++ deploy.sh
@@ -10,20 +10,6 @@ git commit -m 'master'
 git pull origin master
 git push -u origin master
 git push origin master
-
-# 2.打包
-npm run build
-
-# 3.进入打包目录
-cd dist
-
-# 4.发布到gh-pages分支
-git init
-git checkout --orphan gh-pages
-git add .
-git commit -m 'gh-pages'
-git remote add origin https://github.com/zhoubichuan/abstract-vue.git
-git push -f origin gh-pages
-
-# 5.返回初始目录
-cd -
+git pull github master
+git push -u github master
+git push github master
