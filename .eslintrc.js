b0815c3ab02c9a27e872fc0f3508f64d0d38d916
周必川 <zhoubichuan@icloud.com>
2023-05-07 17:47:24 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,25 +2,29 @@
 
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
-    sourceType: 'module'
+    parser: "babel-eslint",
+    sourceType: "module"
   },
   env: {
-    browser: true,
+    browser: true
   },
   // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-  extends: 'standard',
+  extends: "standard",
   // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
+  plugins: ["html"],
   // add your custom rules here
   rules: {
     // allow async-await
-    'generator-star-spacing': 'off',
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'indent': 0
+    "generator-star-spacing": "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    indent: 0,
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false, // 禁止结束符号 “;”
+      },
+    ],
   }
-}
+};
